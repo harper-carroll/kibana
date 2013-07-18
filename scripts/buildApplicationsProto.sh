ec54558b775bbc9c3197e2d36e70bc94097a8e43
Robert Weber <robert.weber@logrhythm.com>
2013-07-18 12:09:51 -0600

    The csve file is weird.  I'm going back to the old way of doing this

--- scripts/buildApplicationsProto.sh
+++ scripts/buildApplicationsProto.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cat resources/Applications.protoheader
-sed -ie 's/, //g' resources/Qosmos_Protocols.csv
-perl scripts/buildApplicationsProto.pl resources/Qosmos_Protocols.csv protofiles/Applications.proto.orig resources/ProtocolFilters  | sort -nk 5
+sed -e 's/, //g' resources/Qosmos_Protocols.csv > resources/Qosmos_Protocols.csv.tmp
+perl scripts/buildApplicationsProto.pl resources/Qosmos_Protocols.csv.tmp protofiles/Applications.proto.orig resources/ProtocolFilters  | sort -nk 5
+rm  resources/Qosmos_Protocols.csv.tmp
 cat resources/Applications.protofooter
