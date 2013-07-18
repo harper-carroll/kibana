cecb842e9eb780e3278f1b5712cca49e33064950
Robert Weber <robert.weber@logrhythm.com>
2013-07-18 11:45:01 -0600

    csv files and commas don't mix, need to pre-sanitize to remove ', '

--- scripts/buildApplicationsProto.sh
+++ scripts/buildApplicationsProto.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cat resources/Applications.protoheader
+sed -ie 's/, //g' resources/Qosmos_Protocols.csv
 perl scripts/buildApplicationsProto.pl resources/Qosmos_Protocols.csv protofiles/Applications.proto.orig resources/ProtocolFilters  | sort -nk 5
 cat resources/Applications.protofooter
