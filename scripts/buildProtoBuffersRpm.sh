f8ef026a9258b92b3c3ab2dcd89410e5d59c4da4
Ben.Aldrich <ben.aldrich@logrhythm.com>
2013-12-17 15:51:13 -0700

    add QA_RPATHS

--- scripts/buildProtoBuffersRpm.sh
+++ scripts/buildProtoBuffersRpm.sh
@@ -13,6 +13,4 @@ rpmdev-setuptree
 cp $PWD/packaging/$PACKAGE.spec ~/rpmbuild/SPECS
 cp thirdParty/protobuf-$VERSION.tar.bz2 ~/rpmbuild/SOURCES
 cd ~/rpmbuild
- QA_RPATHS=$[ 0x0020|0x0002 ] rpmbuild -v -bb --target=x86_64 ~/rpmbuild/SPECS/$PACKAGE.spec
-
-sh scripts/buildProtoFiles.sh
+QA_RPATHS=$[ 0x0020|0x0002 ] rpmbuild -v -bb --target=x86_64 ~/rpmbuild/SPECS/$PACKAGE.spec
