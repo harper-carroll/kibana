3eebd0021262cf5899cdc8031e6e0e0baa579cac
ryan <ryan@ryan-VirtualBox.(none)>
2014-07-07 13:33:38 -0600

    added script to build Events* index config JSON for ES

--- scripts/buildESTemplate.sh
+++ scripts/buildESTemplate.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 ES_TEMPLATE=resources/network.json
+EVENTS_TEMPLATE=resources/events.json
 
 cat resources/elasticsearch_network_template_header.json > $ES_TEMPLATE
 perl scripts/buildESTemplate.pl protofiles/DpiMsgLRproto.proto resources/remapping >> $ES_TEMPLATE
 cat resources/elasticsearch_network_template_footer.json >> $ES_TEMPLATE
+
+cat resources/elasticsearch_events_template_header.json > $EVENTS_TEMPLATE
+perl scripts/buildESTemplate.pl protofiles/DpiMsgLRproto.proto resources/remapping >> $EVENTS_TEMPLATE
+cat resources/elasticsearch_events_template_footer.json >> $EVENTS_TEMPLATE
 
