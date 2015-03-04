44e0fcd3ad158d8a9c62d9db8a284f603e64066c
Ben Aldrich <ben.aldrich@logrhythm.com>
2015-03-04 16:51:21 -0700

    can't use import style, it doesn't take the changes needed for code generation

--- scripts/buildProtoFilesGo.sh
+++ scripts/buildProtoFilesGo.sh
@@ -4,7 +4,7 @@ if [ -z "$GOPATH" ]; then export GOPATH=~/go; fi
 if [ -z "$GOBIN" ]; then export GOBIN=$GOPATH/bin; fi
 if [ -z "$GOROOT" ]; then export GOROOT=/usr/local/go; fi
 startDir=`pwd`
-goSrc=$GOPATH/src/github.schq.secious.com/Logrhythm/GoMessaging/
+goSrc=$GOPATH/src/github.schq.secious.com/Logrhythm/GoMessaging
 goLR=$GOPATH/src/github.schq.secious.com/Logrhythm
 protoFileDir="$startDir"/protofiles
 scriptsDir="$startDir"/scripts
@@ -14,13 +14,17 @@ go get github.com/gogo/protobuf/protoc-gen-gogo
 go get github.com/gogo/protobuf/gogoproto
 
 mkdir -p $goSrc
+mkdir -p $goSrc/stats
 if [ ! -d "$goSrc" ]; then
    cd $goLR
    git clone git@github.schq.secious.com:Logrhythm/GoMessaging.git
    cd $startDir
 fi
+
+(cd "$protoFileDir"/stats; cp "$protoFileDir"/stats/*  "$goSrc"/stats/ )
+(cd "$protoFileDir"; cp `ls  | grep -v Config | grep -v ESData | grep -v RuleConf | grep -v DpiMsgLRproto | grep -v Applications | grep -v BaseConfMsg | grep -v stats`   "$goSrc"/ )
+
+( cd $goSrc; protoc -I="$GOPATH"/src/:/usr/local/include:/usr/include:$goSrc:/$goSrc/stats/ --gogo_out=$GOPATH/src/ "$goSrc"/*.proto )
+( cd "$goSrc"/stats; protoc -I="$GOPATH"/src/:/usr/local/include:/usr/include:$goSrc:$goSrc/stats/ --gogo_out=$GOPATH/src/ "$goSrc"/stats/*.proto )
 rm "$goSrc"/*.proto
-cp `ls "$protoFileDir"/* | grep -v Config | grep -v ESData | grep -v RuleConf | grep -v DpiMsgLRproto | grep -v Applications | grep -v BaseConfMsg `  "$goSrc"/
-cd $goSrc
-protoc -I="$GOPATH"/src/:/usr/local/include:/usr/include:$goSrc/ --gogo_out=$GOPATH/src/ $goSrc/*.proto
-cd "$startDir"
+rm "$goSrc"/stats/*.proto
