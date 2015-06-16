acca5dfd2446224a876d4e38b79ad9b8bbbc0853
John Gress <john.gress@logrhythm.com>
2015-06-16 15:26:55 -0600

    Produce a csv file with qosmos field names for use with Lua documentation.

--- scripts/buildDpiMsgLRProto.pl
+++ scripts/buildDpiMsgLRProto.pl
@@ -2,11 +2,62 @@
 #
 #
 #
+
 use YAML::Any qw(Dump DumpFile);
 
-open qosmosWorkbook, "$ARGV[0]" or die $!;
-open summaryFile, '>'."$ARGV[3]" or die $!;
-seek summaryFile, 0, 0;
+# Read through the resources/Qosmos_Protobook.csv file. Ensure that all values use in the 
+# protofile have rename mappings.
+sub CheckRenameFile {
+   my($qosmosFileName,$includeFilter,$excludeFilter,%renameMapping) = @_;
+   # $staticField_ptr = $_[4]; - this line seems to have a typo (missing 'c')
+   my $staticField_ptr = $_[4];
+
+   my $filename = 'MissingAttributesReport.csv';
+   open(my $missAttrFile, '>', $filename) or die "Could not open file '$filename' $!";
+   print $missAttrFile "remapping file - missing attributes\n";
+   # print $missAttrFile "Application,QProto Name,NetMon Old Name,Short Name,Long Name,Syslog Field,SIEM MPE Tag,SIEM Mapped Field (6.3 Name),SIEM Long Name 6.3,Status,Description\n";
+   print $missAttrFile "Application,QProto Name,NetMon Old Name,Short Name,Long Name,Syslog Field,SIEM MPE Tag,SIEM Mapped Field (6.3 Name),SIEM Long Name 6.3,Status\n";
+   open qosmosWorkbook, "$qosmosFileName" or die $!;
+
+   my $mapGood = 1;
+   while (<qosmosWorkbook>) {
+      if ($_ =~ m/$includeFilter/ && $_ !~ /$excludeFilter/ ) {
+         my @lineValues = parseCsv($_);
+         if ( !defined $renameMapping { $lineValues[8] } &&
+               !defined $renameMapping { "_$lineValues[8]" } ) {
+            # The remapping file needs to have an entry in it for each attribute name in the Qosmos Workbook.
+            # If there are missing attributes, work with Labs to get mappings assigned. Use an updated
+            # NetMonFieldNames.csv file to complete the Protobuffer compilation.
+            my $proto = $lineValues[2];
+            my $proto =~ s/Q_PROTO_//; 
+            my $removedUnderscore = upperCamelCase($lineValues[8]);
+            my $removedSpaces = $removedUnderscore;
+            my @split = $removedSpaces =~ /([A-Z](?:[A-Z]*(?=$|[A-Z][a-z])|[a-z]*))/g;
+            my $scal = join(" ", @split);
+            # print $missAttrFile "$proto,$lineValues[8]$lineValues[2],$lineValues[8],$removedUnderscore,$scal,,,,,New,\"$lineValues[11]\"\n";
+            print $missAttrFile "$proto,$lineValues[8]$lineValues[2],$lineValues[8],$removedUnderscore,$scal,,,,,New,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n";
+            $mapGood = 0;
+         } 
+      } 
+   }
+   close $missAttrFile;
+
+   if ($mapGood) {
+      while (my($key,$value) = each (%renameMapping)) {
+         my @matches =  grep { /$value/ } @$staticField_ptr; # use strict has a problem with this line
+         if (@matches && $matches[0] eq $value ) {
+            die "Rename file tries to map to a static field $value, $matches[0]";
+         }
+      }
+   } else {
+      die "**** BUILD FAILED ****\nNetMonFieldNames.csv needs to be updated by labs.\nSee $filename for list of missing attributes.\n";
+   }
+
+   close qosmosWorkbook;
+
+}
+
+use strict; # Trying to get more strict.
 
 sub ReadFilters {
    my($filename) = @_;
@@ -43,13 +94,13 @@ sub ReadRemappingFile {
 
 sub ReadPreviousData {
    my($filename) = $_[0];
-   $highest_ptr = $_[1];
-   $ids_ptr = $_[2];
-   $callbackNames_ptr = $_[3];
-   $previousFields_ptr = $_[4];
-   $previousData_ptr = $_[5];
+   my $highest_ptr = $_[1];
+   my $ids_ptr = $_[2];
+   my $callbackNames_ptr = $_[3];
+   my $previousFields_ptr = $_[4];
+   my $previousData_ptr = $_[5];
    # $statiField_ptr = $_[6]; - this line seems to have a typo (missing 'c')
-   $staticField_ptr = $_[6];
+   my $staticField_ptr = $_[6];
 
    $$highest_ptr = 1;
    $$callbackNames_ptr = ",";
@@ -98,102 +149,9 @@ sub parseCsv {
    return @new; ## list of values that were comma-spearated
 }
 
-# Create a file with the set of protocols and attributes supported by the protobundle.
-sub CreateSummaryFile {
-
-   # Switch the order of parameters for excludeFilter and includeFilter. The call has the 
-   # order reversed: See "CreateSummaryFile($QosmosWorkBookName,$ARGV[3],$excludeFilter,$includeFilter);"
-   my($qosmosFileName,$summaryFileName,$excludeFilter,$includeFilter) = @_;
-
-   open qosmosWorkbook, "$qosmosFileName" or die $!;
-   open summaryFile, '>'."$ARGV[3]" or die $!;
-   seek summaryFile, 0, 0;
-
-   print summaryFile "protocolName,longProtocolName,attributeName,attributeDescription\n";
-   while (<qosmosWorkbook>) {
-      if ($_ =~ m/$includeFilter/ && $_ !~ /$excludeFilter/ ) {
-         my @lineValues = parseCsv($_);
-         if ($lineValues[4] eq '' ) {
-            print summaryFile "base,";
-         } else {
-            print summaryFile "$lineValues[4],";
-         }
-         if ($lineValues[5] eq '' ) {
-            print summaryFile "base,";
-         } else {
-            print summaryFile "$lineValues[5],";
-         }
-         print summaryFile '#'."$lineValues[8],";
-         # Put quotes around descriptions which contain a comma
-         my $commaFound = index($lineValues[11], ',');
-         if ($commaFound != -1) {
-            print summaryFile "\"$lineValues[11]\"\n";
-         } else {
-            print summaryFile "$lineValues[11]\n";
-         }
-      }
-   }
-
-   close qosmosWorkbook;
-   close summaryFile;
-}
-
 sub upperCamelCase {
    join '', map ucfirst, split '_', $_[0];
 }
-
-# Read through the resources/Qosmos_Protobook.csv file. Ensure that all values use in the 
-# protofile have rename mappings.
-sub CheckRenameFile {
-   my($qosmosFileName,$includeFilter,$excludeFilter,%renameMapping) = @_;
-   # $staticField_ptr = $_[4]; - this line seems to have a typo (missing 'c')
-   $staticField_ptr = $_[4];
-
-   my $filename = 'MissingAttributesReport.csv';
-   open(my $missAttrFile, '>', $filename) or die "Could not open file '$filename' $!";
-   print $missAttrFile "remapping file - missing attributes\n";
-   # print $missAttrFile "Application,QProto Name,NetMon Old Name,Short Name,Long Name,Syslog Field,SIEM MPE Tag,SIEM Mapped Field (6.3 Name),SIEM Long Name 6.3,Status,Description\n";
-   print $missAttrFile "Application,QProto Name,NetMon Old Name,Short Name,Long Name,Syslog Field,SIEM MPE Tag,SIEM Mapped Field (6.3 Name),SIEM Long Name 6.3,Status\n";
-   open qosmosWorkbook, "$qosmosFileName" or die $!;
-
-   $mapGood = 1;
-   while (<qosmosWorkbook>) {
-      if ($_ =~ m/$includeFilter/ && $_ !~ /$excludeFilter/ ) {
-         my @lineValues = parseCsv($_);
-         if ( !defined $renameMapping { $lineValues[8] } &&
-               !defined $renameMapping { "_$lineValues[8]" } ) {
-            # The remapping file needs to have an entry in it for each attribute name in the Qosmos Workbook.
-            # If there are missing attributes, work with Labs to get mappings assigned. Use an updated
-            # NetMonFieldNames.csv file to complete the Protobuffer compilation.
-            $proto = $lineValues[2];
-            $proto =~ s/Q_PROTO_//; 
-            $removedUnderscore = upperCamelCase($lineValues[8]);
-            $removedSpaces = $removedUnderscore;
-            my @split = $removedSpaces =~ /([A-Z](?:[A-Z]*(?=$|[A-Z][a-z])|[a-z]*))/g;
-            $scal = join(" ", @split);
-            # print $missAttrFile "$proto,$lineValues[8]$lineValues[2],$lineValues[8],$removedUnderscore,$scal,,,,,New,\"$lineValues[11]\"\n";
-            print $missAttrFile "$proto,$lineValues[8]$lineValues[2],$lineValues[8],$removedUnderscore,$scal,,,,,New,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n";
-            $mapGood = 0;
-         } 
-      } 
-   }
-   close $missAttrFile;
-
-   if ($mapGood) {
-      while (($key,$value) = each (%renameMapping)) {
-         my @matches =  grep { /$value/ } @$staticField_ptr;
-         if (@matches && $matches[0] eq $value ) {
-            die "Rename file tries to map to a static field $value, $matches[0]";
-         }
-      }
-   } else {
-      die "**** BUILD FAILED ****\nNetMonFieldNames.csv needs to be updated by labs.\nSee $filename for list of missing attributes.\n";
-   }
-
-   close qosmosWorkbook;
-
-}
-
 
 sub GetStartOfIpp {
    my $fileContents = 
@@ -229,8 +187,8 @@ sub CreateRemappingFile {
    seek remappingFile, 0, 0; # Set file handle position to beginning of file
    seek ippFile, 0, 0;
 
-   $header = GetStartOfIpp();
-   $footer = GetEndOfIpp();
+   my $header = GetStartOfIpp();
+   my $footer = GetEndOfIpp();
    print ippFile "$header";
 
    while (<nmfieldnamesFile>) {
@@ -256,22 +214,22 @@ sub CreateRemappingFile {
 #ARGV[0] is resources/Qosmos_Protobook.csv 
 #ARGV[1] is protofiles/DpiMsgLRproto.proto.orig 
 #ARGV[2] is resources/ProtocolFilters 
-#ARGV[3] is resources/ProtocolDescriptions.csv 
+#ARGV[3] is resources/LuaProtoFieldDesc.csv 
 #ARGV[4] is resources/remapping 
 #ARGV[5] is resources/remapping.ipp 
 #ARGV[6] is resources/NetMonFieldNames.csv 
 #ARGV[7] is /tmp/buildDpiMsgLRProto2.$BASHPID
 
-$QosmosWorkBookName = $ARGV[0];
+my $QosmosWorkBookName = $ARGV[0];
 
 # Load a set of regex strings from resources/ProtocolFilters
-($excludeFilter,$includeFilter) = ReadFilters($ARGV[2]);
+my ($excludeFilter,$includeFilter) = ReadFilters($ARGV[2]);
 
 # Update the remapping file using, 4 = resources/remapping, 6 = resources/NetMonFieldNames.csv
 CreateRemappingFile($ARGV[4],$ARGV[6], $ARGV[5]);
 
 # Get the new mapping of names to use
-(%renameMapping) = ReadRemappingFile($ARGV[4]);
+my(%renameMapping) = ReadRemappingFile($ARGV[4]);
 my $renameMap = $ARGV[5]; # 5 = resource/remapping.yaml
 if ($renameMap eq "" ) {
    die "Must name a path to output rename map $ARGV[5] $renameMap" ;
@@ -288,17 +246,13 @@ my @previousData;
 my @staticFields;
 ReadPreviousData($ARGV[1],\$highest, \@ids, \$callbackNames, \@previousFields, \@previousData, \@staticFields);
 
-# Write out the set of protocols and attributes supported by the protobundle (3 = resources/ProtocolDescriptions.csv).
-# This output file should be provided to Labs and marketing.
-CreateSummaryFile($QosmosWorkBookName,$ARGV[3],$excludeFilter,$includeFilter);
-
 # Check to make sure every field in the resources/Qosmos_Protobook.csv has a remapping assigned to it.
 CheckRenameFile($QosmosWorkBookName,$includeFilter,$excludeFilter,%renameMapping,\@staticFields);
 
 # Create a temporary file at /tmp/buildDpiMsgLRProto2.$BASHPID. This is a scratch file used
 # to sort the body contents of the DPI message by enum ID. A header and footer are added around
 # the body contents.
-open(my $dpiMsgProtoBody, '>', $ARGV[7]) or die "Could not open file '$filename' $!";
+open(my $dpiMsgProtoBody, '>', $ARGV[7]) or die "Could not open file '$ARGV[7]' $!";
 seek $dpiMsgProtoBody, 0, 0; # Set file handle position to beginning of file
 
 # Open resources/Qosmos_Protobook.csv and save the previously supported attributes
@@ -306,8 +260,8 @@ open qosmosWorkbook, "$ARGV[0]" or die $!;
 while ( my $line = <qosmosWorkbook>) {
    # The split on comma works here since the description field, which sometimes contains a comma,
    # is in column 11 and not used here.
-   @lineValues = split(/,/,$line);
-   $field = "$lineValues[8]$lineValues[2]";
+   my @lineValues = split(/,/,$line);
+   my $field = "$lineValues[8]$lineValues[2]";
    my $index = 0;
    foreach (@previousData) {
       if ( $_ =~ /$field/ ) {
@@ -322,46 +276,49 @@ while ( my $line = <qosmosWorkbook>) {
    }
 }
 
-seek qosmosWorkbook, 0, 0;
-#print @previousData;
 
+# Create a resource/LuaProtoFieldDesc.csv file
+open luaProtoFieldsCsv, '>'."$ARGV[3]" or die $!;
+seek luaProtoFieldsCsv, 0, 0;
+print luaProtoFieldsCsv "Protocol Name, Long Protocol Name, Attribute Name, Full Attribute Name, Attribute Type, Attribute Description\n";
+
+seek qosmosWorkbook, 0, 0;
 while (<qosmosWorkbook>) {
    # Include all attributes matching the includeFilter, but exclude the 19 attributes at the beginning 
    # of the Qosmos_Protobook.
    if ($_ =~ m/$includeFilter/ && $_ !~ ",\.,,,,," ) {
-      # The split on comma works here since the description field, which sometimes contains a comma,
-      # is in column 11 and not used here.
-      @lineValues = split(/,/,$_);
-      $field = "$lineValues[8]$lineValues[2]";
+      my @lineValues = parseCsv($_);
+      my $field = "$lineValues[8]$lineValues[2]";
       if ( $field =~ /^[0-9]/ ) {
          $field = "_$lineValues[8]$lineValues[2]";
       }
+      my $requirement = "optional";
+      my $type = $lineValues[10];
+      my $optionalStuff = "";
+
+      if ($lineValues[10] =~ /timeval/) {
+         $type = "string";
+         $optionalStuff = ",timeval,timevalToString";
+      } elsif ($lineValues[10] =~ /ip_addr/) {
+         $type = "string";
+         $optionalStuff = ",uint32,ip_addrToString";
+      } elsif ($lineValues[10] =~ /mac_addr/) {
+         $type = "string";
+         $optionalStuff = ",clep_mac_addr_t,mac_addrToString";
+      } elsif ($lineValues[10] eq "") {
+         print $ARGV[0]." MALFORMED FILE!!!!\n";
+         print $ARGV[0]." 0:$lineValues[1],1:$lineValues[2],2:$lineValues[2],3:lineValues[4],4:$lineValues[5],5:$lineValues[6],6:$lineValues[7],7:$lineValues[8],8:$lineValues[9],9:$lineValues[10]\n";
+         exit(1);
+      } elsif ($lineValues[10] =~ /string/) {
+         $type = "bytes";
+         $requirement = "repeated";
+      } elsif ($lineValues[10] =~ /parent/) {
+         $type = "bool";
+      } elsif ($lineValues[10] =~ /ptr/) {
+         $type = "Void";
+      }
       if ($callbackNames !~ /,$field,/ ) {
-         $requirement = "optional";
          $highest += 1;
-         $type = $lineValues[10];
-         $optionalStuff = "";
-         if ($lineValues[10] =~ /timeval/) {
-            $type = "string";
-            $optionalStuff = ",timeval,timevalToString";
-         } elsif ($lineValues[10] =~ /ip_addr/) {
-            $type = "string";
-            $optionalStuff = ",uint32,ip_addrToString";
-         } elsif ($lineValues[10] =~ /mac_addr/) {
-            $type = "string";
-            $optionalStuff = ",clep_mac_addr_t,mac_addrToString";
-         } elsif ($lineValues[10] eq "") {
-            print $ARGV[0]." MALFORMED FILE!!!!\n";
-            print $ARGV[0]." 0:$lineValues[1],1:$lineValues[2],2:$lineValues[2],3:lineValues[4],4:$lineValues[5],5:$lineValues[6],6:$lineValues[7],7:$lineValues[8],8:$lineValues[9],9:$lineValues[10]\n";
-            exit(1);
-         } elsif ($lineValues[10] =~ /string/) {
-            $type = "bytes";
-            $requirement = "repeated";
-         } elsif ($lineValues[10] =~ /parent/) {
-            $type = "bool";
-         } elsif ($lineValues[10] =~ /ptr/) {
-            $type = "Void";
-         }
          if ($_ !~ /$excludeFilter/ ) {
             # Protobuffer output; add new attribute.
             print $dpiMsgProtoBody "$requirement $type $field = $highest; // QOSMOS:$lineValues[2],$lineValues[7]$optionalStuff\n";
@@ -370,7 +327,12 @@ while (<qosmosWorkbook>) {
             print $dpiMsgProtoBody "//$requirement $type $field = $highest; // QOSMOS:$lineValues[2],$lineValues[7]$optionalStuff\n";
          }
       }
+      if ($_ !~ /$excludeFilter/ ) {
+         print luaProtoFieldsCsv "$lineValues[4], $lineValues[5], $lineValues[8], $field, $type, \"$lineValues[11]\"\n";
+      }
    }
 }
 
+close qosmosWorkbook;
+close luaProtoFieldsCsv;
 close $dpiMsgProtoBody;
--- scripts/buildDpiMsgLRProto.sh
+++ scripts/buildDpiMsgLRProto.sh
@@ -4,7 +4,7 @@ TMPFILE=/tmp/buildDpiMsgLRProto.$BASHPID
 TMPFILE2=/tmp/buildDpiMsgLRProto2.$BASHPID
 
 cat resources/DpiMsgLRproto.protoheader > $TMPFILE
-perl scripts/buildDpiMsgLRProto.pl resources/Qosmos_Protobook.csv protofiles/DpiMsgLRproto.proto.orig resources/ProtocolFilters resources/LuaProtoFieldDesc.csv resources/remapping cpp/source/liblrdpi/remapping.ipp resources/NetMonFieldNames.csv $TMPFILE2
+perl scripts/buildDpiMsgLRProto.pl resources/Qosmos_Protobook.csv protofiles/DpiMsgLRproto.proto.orig resources/ProtocolFilters resources/LuaProtoFieldsDesc.csv resources/remapping cpp/source/liblrdpi/remapping.ipp resources/NetMonFieldNames.csv $TMPFILE2
 # Sort the DPI Message contents by the enumeration ID (field 5)
 sort -nk 5 $TMPFILE2 >> $TMPFILE
 cat resources/DpiMsgLRproto.protofooter >> $TMPFILE
