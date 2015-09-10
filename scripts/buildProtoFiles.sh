e048acddb8cb1d9cbefab5d6920ce20c8e12b0ab
Yannick.Polius <Yannick.Polius@logrhythm.com>
2015-09-10 15:12:55 -0600

    buildProtoFiles.sh: fixed typo in build script

--- scripts/buildProtoFiles.sh
+++ scripts/buildProtoFiles.sh
@@ -112,7 +112,7 @@ mv  libprocess/ProcessRequest.pb.cc  libprocess/ProcessRequest.pb.cpp
 mv  libprocess/ProcessReply.pb.cc  libprocess/ProcessReply.pb.cpp
 mv  libcommand/DriveInfo.pb.cc  libcommand/DriveInfo.pb.cpp
 mv  libcommand/LuaDefinedRule.pb.cc  libcommand/LuaDefinedRule.pb.cpp
-mv  libcommand/DeepScriptIOIOMsg.pb.cc  libcommand/DeepScriptIOMsg.pb.cpp
+mv  libcommand/DeepScriptIOMsg.pb.cc  libcommand/DeepScriptIOMsg.pb.cpp
 
 mv  libcommand/ConfigDefaults.pb.cc  libcommand/ConfigDefaults.pb.cpp
 mv  libcommand/ConfigDefaultsRequest.pb.cc  libcommand/ConfigDefaultsRequest.pb.cpp
