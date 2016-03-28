7a3a4de7fa9e08a49338ed156fa6d79f0db5b237
matt <matt.petersen@logrhythm.com>
2016-03-28 14:03:10 -0600

    update script to correctly remove .proto files which exist in GoMessaging which none ever should

--- scripts/buildProtoFilesGo.sh
+++ scripts/buildProtoFilesGo.sh
@@ -4,7 +4,7 @@ if [ -z "$GOPATH" ]; then export GOPATH=~/go; fi
 if [ -z "$GOBIN" ]; then export GOBIN=$GOPATH/bin; fi
 if [ -z "$GOROOT" ]; then export GOROOT=/usr/local/go; fi
 startDir=`pwd`
-goSrc=$GOPATH/src/github.schq.secious.com/Logrhythm/GoMessaging
+goSrc=$GOPATH/src/github.schq.secious.com/Logrhythm/GoMessaging_Gen
 goLR=$GOPATH/src/github.schq.secious.com/Logrhythm
 protoFileDir="$startDir"/protofiles
 scriptsDir="$startDir"/scripts
@@ -21,7 +21,7 @@ echo "Running 'go get' dependencies this script requires"
 go get gopkg.in/yaml.v2 # required by rewriteProto
 go install github.schq.secious.com/Logrhythm/rewriteProto
 go get github.com/LogRhythm/protobuf/proto
-#go get github.com/gogo/protobuf || true # no buildable source (exits with non-zero)
+go get github.com/gogo/protobuf || true # no buildable source (exits with non-zero)
 
 
 mkdir -p $goSrc # Gauranteeing GoMessaging is a real directory
@@ -29,13 +29,8 @@ mkdir -p $goSrc # Gauranteeing GoMessaging is a real directory
 echo "Removing pre-existing .proto files in $goSrc"
 (
 cd $goSrc;
-for d in */ ; do
-  if ["$d" -ne "vendor/"]; then
-    rm $goSrc/"$d"*.proto || true
-  fi
-done
+rm -rf $(find * -name '*.proto' | grep -v 'vendor/')
 )
-
 # rewriteProto process proto files for use with gogoprotobuf and deposits the result in $goSrc
 # which are then compiled into .pb.go files by protoc, etc below
 echo "Processing .proto files from Protobuffers repo into GoMessaging to add gogoprotobuf extensions"
@@ -49,5 +44,5 @@ find * -type d -exec /usr/bin/sh -c "rm $goSrc/{}/*.proto" \;
 
 echo "Compile all main level protos"
 protoc -I=$GOPATH/src/:/usr/local/include:/usr/include:$goSrc --gogo_out=$GOPATH/src/  $goSrc/*.proto
-rm $goSrc/*.proto
+rm -rf $(find * -name '*.proto' | grep -v 'vendor/')
 )
