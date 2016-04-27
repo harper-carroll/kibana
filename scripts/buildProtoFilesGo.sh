7fc007cb9da6adb7d14c4ac64a0dc5d67c4cbc89
Tom Artale <tom.artale@usmac1456tarta.schq.secious.com>
2016-04-27 15:56:22 -0600

    Modified script to use a frozen gogoproto, and work on a system with an alternate protoc install location

--- scripts/buildProtoFilesGo.sh
+++ scripts/buildProtoFilesGo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
+
+if [ -z "$PROTOINCLUDE" ]; then export PROTOINCLUDE=/usr/local/include; fi
 if [ -z "$GOPATH" ]; then export GOPATH=~/go; fi
 if [ -z "$GOBIN" ]; then export GOBIN=$GOPATH/bin; fi
 if [ -z "$GOROOT" ]; then export GOROOT=/usr/local/go; fi
@@ -12,19 +14,17 @@ thirdPartyDir="$startDir"/thirdParty
 
 # Seems as though we should be able to do a `go get` for this repo, but it
 # reports 'unrecognized import path'
-if [ ! -d $goLR/rewriteProto ]; then
-  echo "Cloning http://github.schq.secious.com/Logrhythm/rewriteProto.git"
-  git clone http://github.schq.secious.com/Logrhythm/rewriteProto.git $goLR/rewriteProto
-fi
+#if [ ! -d $goLR/rewriteProto ]; then
+#  echo "Cloning http://github.schq.secious.com/Logrhythm/rewriteProto.git"
+#  git clone http://github.schq.secious.com/Logrhythm/rewriteProto.git $goLR/rewriteProto
+#fi
 
 echo "Running 'go get' dependencies this script requires"
-go get gopkg.in/yaml.v2 # required by rewriteProto
+go get github.schq.secious.com/Logrhythm/rewriteProto
 go install github.schq.secious.com/Logrhythm/rewriteProto
-go get github.com/LogRhythm/protobuf/proto
-go get github.com/gogo/protobuf || true # no buildable source (exits with non-zero)
+go get github.schq.secious.com/DataIndexer/GoGoProtobuf || true # ignore 'no buildable source' non-zero exit
 
-
-mkdir -p $goSrc # Gauranteeing GoMessaging is a real directory
+mkdir -p $goSrc # Guaranteeing GoMessaging is a real directory
 
 echo "Removing pre-existing .proto files in $goSrc"
 (
@@ -39,10 +39,10 @@ echo "Processing .proto files from Protobuffers repo into GoMessaging to add gog
 echo "Running Protoc"
 (
 cd $goSrc;
-find * -type d -exec /usr/bin/sh -c "protoc -I=$GOPATH/src/:/usr/local/include:/usr/include:$goSrc --gogo_out=$GOPATH/src/  $goSrc/{}/*.proto" \;
-find * -type d -exec /usr/bin/sh -c "rm $goSrc/{}/*.proto" \;
+find * -type d -exec /bin/sh -c "protoc -I=$GOPATH/src/:$PROTOINCLUDE:/usr/local/include:/usr/include:$goSrc --gogo_out=$GOPATH/src/  $goSrc/{}/*.proto" \;
+find * -type d -exec /bin/sh -c "rm $goSrc/{}/*.proto" \;
 
 echo "Compile all main level protos"
-protoc -I=$GOPATH/src/:/usr/local/include:/usr/include:$goSrc --gogo_out=$GOPATH/src/  $goSrc/*.proto
+protoc -I=$GOPATH/src/:$PROTOINCLUDE:/usr/local/include:/usr/include:$goSrc --gogo_out=$GOPATH/src/  $goSrc/*.proto
 rm -rf $(find * -name '*.proto' | grep -v 'vendor/')
 )
