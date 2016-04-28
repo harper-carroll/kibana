5c3e210045e7b55b9a7578cbec23aaa552936449
Tom Artale <tom.artale@usmac1456tarta.schq.secious.com>
2016-04-28 16:22:45 -0600

    Make this correctly install protobuf on both centos and Mac

--- scripts/compileThirdParty.sh
+++ scripts/compileThirdParty.sh
@@ -7,7 +7,7 @@ DISTDIR="$thirdPartyDir"/$DIST
 protoInstallDir="$DISTDIR"/protobuf/
 protoc="$protoInstallDir"/bin/protoc
 PROTOBUFFER_VERSION=2.6.1
-GLOBAL_CPP_FLAGS="-fPIC -Ofast -m64 -flto "
+#GLOBAL_CPP_FLAGS="-fPIC -Ofast -m64 -flto "
 PATH=/usr/local/probe/bin:$PATH
 
 rm -rf "$DISTDIR"/protobuf
@@ -15,6 +15,14 @@ rm -rf "$thirdPartyDir"/protobuf-$PROTOBUFFER_VERSION
 cd "$thirdPartyDir"
 tar xvzf protobuf-$PROTOBUFFER_VERSION.tar.gz
 cd protobuf-$PROTOBUFFER_VERSION
+
+os=`uname`
+if [ "$os" == "Linux" ]
+then
+   sudo yum -y install autoconf automake libtool
+   ./autogen.sh
+fi
+
 CPPFLAGS="$CPPFLAGS $GLOBAL_CPP_FLAGS" LDFLAGS=$GLOBAL_CPP_FLAGS ./configure --prefix="$DISTDIR"/protobuf
 make -j
 make install
